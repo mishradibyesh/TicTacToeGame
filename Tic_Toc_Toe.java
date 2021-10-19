a49122754f3f734076cebcf0a43b42f16594a5a5
mishradibyesh <dibyeshmishra207@gmail.com>
2021-10-19 15:07:51 +0530

    player making move

--- Tic_Toc_Toe.java
+++ Tic_Toc_Toe.java
@@ -4,24 +4,25 @@ public class Tic_Toc_Toe {
 	static char[] board =new char[10];
 	static char Player_letter;
 	static char Computer_letter;
+	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 		System.out.println("welcome to Tic Tac Toi GAME") ;
 		board() ; //calling the method board in main method	
 		chooseLetter();
 		showBoard();
+		play();
+		showBoard();
 	}
 	public static void board() 
-	{
-		// giving each array index value as empty initially
+	{		                                    // creating board to play  
 		for(int i=1;i < 10 ; i++) {
 			board[i] =' ';
 		}
 
 	}
 	public static void chooseLetter() {
-		Scanner s = new Scanner(System.in);
 		System.out.println("please  select X  or O ");
-		char letter = s.next().charAt(0);       // allowing user to choose character using console
+		char letter = sc.next().charAt(0);       // allowing user to choose character using console
 		if(letter=='X') {
 			Player_letter=letter;
 			Computer_letter='O';
@@ -32,8 +33,8 @@ public class Tic_Toc_Toe {
 		}
 		else {                                  // if user is entering wrong choice , default letter will be allocated
 			System.out.println("wrong selection ! Now computer will assign letters.");
-			Computer_letter = 'X';
-			Player_letter='O';
+			Computer_letter = 'O';
+			Player_letter ='X';
 		}
 		System.out.println("player letter is "+Player_letter);
 		System.out.println("computer letter is "+Computer_letter);
@@ -47,5 +48,17 @@ public class Tic_Toc_Toe {
 		System.out.println(" "+board[7]+" |"+" "+board[8]+" |"+" "+board[9]);
 
 	}
-
+	public static void play() {
+	System.out.println("Enter the place from 1 to 9 for your next move ");
+	int place = sc.nextInt();
+	if(place < 10 && place > 0 ) {
+        if(board[place] == ' ') {
+        	board[place] = Player_letter;
+        }
+        else
+        	System.out.println(" this place is already occupied ! please select another ");
+	}
+	else
+		System.out.println("wrong place entered ! please select nos between 1 to 9 only ");
+	}
 }
