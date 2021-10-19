d4c38ba8126fd244db9db283569fc572a34c6e9c
mishradibyesh <dibyeshmishra207@gmail.com>
2021-10-19 14:04:42 +0530

    ability to choose letter using console

--- Tic_Toc_Toe.java
+++ Tic_Toc_Toe.java
@@ -1,9 +1,13 @@
 package com.bl.tictoctoe.game;
+import java.util.Scanner;
 public class Tic_Toc_Toe {
 	static char[] board =new char[10];
+	static char Player_letter;
+	static char Computer_letter;
 	public static void main(String[] args) {
 		System.out.println("welcome to Tic Tac Toi GAME") ;
-		board() ;                            //calling the method board in main method	
+		board() ; //calling the method board in main method	
+		chooseLetter();
 	}
 	public static void board() 
 	{
@@ -12,6 +16,26 @@ public class Tic_Toc_Toe {
 			board[i] =' ';
 		}
 
+	}
+	public static void chooseLetter() {
+		Scanner s = new Scanner(System.in);
+		System.out.println("please  select X  or O ");
+		char letter = s.next().charAt(0);       // allowing user to choose character using console
+		if(letter=='X') {
+			Player_letter=letter;
+			Computer_letter='O';
+		}
+		else if(letter == 'O') {
+			Player_letter = letter;
+			Computer_letter = 'X';
+		}
+		else {                                  // if user is entering wrong choice , default letter will be allocated
+			System.out.println("wrong selection ! Now computer will assign letters.");
+			Computer_letter = 'X';
+			Player_letter='O';
+		}
+		System.out.println("player letter is "+Player_letter);
+		System.out.println("computer letter is "+Computer_letter);
 	}
 
 }
