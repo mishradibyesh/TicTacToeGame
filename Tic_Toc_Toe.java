7daa213eea60c36df55a615b3c7df3d8f39127a2
mishradibyesh <dibyeshmishra207@gmail.com>
2021-10-19 13:32:29 +0530

    creating board

--- Tic_Toc_Toe.java
+++ Tic_Toc_Toe.java
@@ -1,6 +1,17 @@
 package com.bl.tictoctoe.game;
 public class Tic_Toc_Toe {
+	static char[] board =new char[10];
 	public static void main(String[] args) {
 		System.out.println("welcome to Tic Tac Toi GAME") ;
+		board() ;                            //calling the method board in main method	
 	}
+	public static void board() 
+	{
+		// giving each array index value as empty initially
+		for(int i=1;i < 10 ; i++) {
+			board[i] =' ';
+		}
+
+	}
+
 }
